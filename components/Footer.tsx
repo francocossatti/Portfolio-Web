64f128c9e282a0312eeb2ab9e38ef156f1e84bb3
Franco Cossatti <frcosati@hotmail.com>
2024-01-28 19:11:37 -0300

    fixed some things

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,18 +5,17 @@ import {
 } from "react-icons/ai";
 
 const Footer = () => {
-
   return (
     <footer className="w-full mx-auto px-4 sm:px-6">
       <hr className="w-full h-0.5 mx-auto mt-8 bg-neutral-200 border-0"></hr>
-      <div className="mx-auto p-4 flex flex-col text-center text-neutral-900 md:flex-row md:justify-between">
-        <div className={`flex flex-row items-center justify-center space-x-1 `}>
-          © 2023 Franco Cossatti<a href="/" className="hover:underline"></a>
+      <div className="mx-auto  p-4 flex flex-col text-center md:flex-row md:justify-between">
+        <div className="flex flex-row items-center justify-center space-x-1">
+          <a href="/" className="hover:underline">© 2024 Franco Cossatti</a>
         </div>
         <div className="flex flex-row items-center justify-center space-x-2 mb-1">
           <a href="https://github.com/francocossatti" rel="noreferrer" target="_blank">
             <AiOutlineGithub
-              className={`hover:-translate-y-1 transition-transform cursor-pointer`}
+              className="hover:-translate-y-1 transition-transform cursor-pointer"
               size={30}
             />
           </a>
@@ -26,7 +25,7 @@ const Footer = () => {
             target="_blank"
           >
             <AiOutlineLinkedin
-              className={`hover:-translate-y-1 transition-transform cursor-pointer`}
+              className="hover:-translate-y-1 transition-transform cursor-pointer"
               size={30}
             />
           </a>
